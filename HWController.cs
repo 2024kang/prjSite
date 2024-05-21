186f2aa2dcc378ea0b494f1dfc7a0444c25a4d49
2024kang <166598158+2024kang@users.noreply.github.com>
2024-05-22 00:23:27 +0800

    Add files via upload

---
+++ HWController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace prjSite2.Controllers
+{
+    public class HWController : Controller
+    {
+        public IActionResult HW1()
+        {
+            return View();
+  
+        }
+
+       
+    }
+}
